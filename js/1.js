ec247dc806231c7776a4c67e04990b9f2e31100d
Mr-Green-1 <alex.grinevsky@gmail.com>
2021-11-17 13:55:33 +0100

    added catname

--- js/1.js
+++ js/1.js
@@ -1,17 +1,29 @@
 
-var a;
-a = 7;
-var b;
-b = a;
+// var a;
+// a = 7;
+// var b;
+// b = a;
 
-var myFirstName = "alex";
-var myLastName = "green";
+// var myFirstName = "alex";
+// var myLastName = "green";
 
 
-var a = 5;
-var b = 10;
-var c = "I am a";
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
