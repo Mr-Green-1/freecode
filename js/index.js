4f13deb5c9dba2b66be3527295a6e7d73ba5b031
A.Green <alex.grinevsky@gmail.com>
2022-05-04 15:24:19 +0200

    added longest

--- js/index.js
+++ js/index.js
@@ -228,6 +228,17 @@ function digitize(n) {
   return String(n).split('').reverse().map(Number);
 }
 
+function longest(s1, s2) {
+  let a = [... new Set(s1.split(""))];
+  let b = [... new Set(s2.split(""))];
+  for (let i = 0; i < b.length; i++) {
+    a.push(b[i]);
+  }
+  let init_combo = [... new Set(a)];
+  let combo = init_combo.sort().join("");
+  return combo;
+}
+
 
 
 
