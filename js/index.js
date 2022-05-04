9983a5d236f6ca934d72891d037fce884a33343c
A.Green <alex.grinevsky@gmail.com>
2022-05-04 11:35:56 +0200

    added js

--- js/index.js
+++ js/index.js
@@ -179,6 +179,9 @@ function stringToArray(string) {
   return string.split(' ')
 }
 
+function openOrSenior(data) {
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
 
 
 
